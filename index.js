4307fa20c2cc80fb3936acacfbb780e4e45d98f8
Teuton Roy <popeyehk404@gmaill.com>
2023-01-09 18:57:14 +0530

    import  section

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 //import express//
-// const express = require('express');
+// const express = require('express'); => In general we are import like this//
+// But here, I'm import like react. for this we have litile-bit change into package.json file("type":"module")//
+
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
